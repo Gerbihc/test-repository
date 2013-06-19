5be0793e78421b47e12c81e3d1b86e3442c65968
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-06-19 11:33:20 -0500

    Refactored class

--- example.rb
+++ example.rb
@@ -1,11 +1,12 @@
 class BookInStock
   attr_accessor :isbn, :price
-  def initialize(isbn, price)
-    @isbn = isbn
-    @price = price
+  def initialize(arguments={})
+    @isbn = arguments[:isbn]
+    @price = arguments[:price]
   end
 
   def to_s
     "ISBN: #{@isbn}, Price: #{@price}"
   end
 end
+BookInStock.new({isbn: 'algo', price: 'algo'})
---
+++ hashes.rb
@@ -0,0 +1,4 @@
+# Hashes are simple structures that provides you a way to define "key, value" type variables
+h = {price: "valor", isbn: 'Otro valor'}
+h[:price] # returns price value
+h[:isbn] # returns isbn value
