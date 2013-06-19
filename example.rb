faf8cbc9f3003431b38acbd655ab4f1b7e54ec28
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-06-19 09:41:24 -0500

    Added variables example

---
+++ example.rb
@@ -0,0 +1,5 @@
+variable = "algo" # Local variable
+@variable = "Otro valor" # Instance Variable
+@@variable = "Class value" # Class Variable
+$variable = "Global value" # Global variable
+VARIABLE = "Constant value" # Constant
